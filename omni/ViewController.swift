ba9740a00b64b8e9a8c4c9388da9cee22348f014
michaelfarinacci <michael@viptela.com>
2014-08-02 22:38:21 -0700

    Initial commit from local git

---
+++ omni/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  omni
+//
+//  Created by Michael Farinacci on 8/2/14.
+//  Copyright (c) 2014 omni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
